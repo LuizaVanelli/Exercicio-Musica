1bd3591379af1224d1f4263911983a2b19b27cbe
LuizaVanelli <luizavanelli.vanelli@gmail.com>
2021-08-28 21:56:14 -0300

    Add atividade musica

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercicio_Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.WriteLine("Put your head on my shoulder");
+            Console.WriteLine("Paul Anka");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.White;
+            Console.WriteLine("Put your head on my shoulder");
+            Console.WriteLine("Hold me in your arms, baby");
+            Console.WriteLine("Squeeze me oh-so-tight");
+            Console.WriteLine("Show me that you love me too");
+            Console.WriteLine();
+            Console.WriteLine("Put your lips next to mine, dear");
+            Console.WriteLine("Won't you kiss me once, baby?");
+            Console.WriteLine("Just a kiss goodnight, maybe");
+            Console.WriteLine("You and I will fall in love (you and I will fall in love)");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.Blue;
+            Console.WriteLine("People say that loves a game");
+            Console.WriteLine("A game you just can't win");
+            Console.WriteLine("If there's a way");
+            Console.WriteLine("I'll find it somebody");
+            Console.WriteLine("And then this fool will rush in");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.White;
+            Console.WriteLine("Put your head on my shoulder");
+            Console.WriteLine("Whisper in my ear, baby");
+            Console.WriteLine("Words I want to hear");
+            Console.WriteLine("Tell me, tell me that you love me too (tell me that you love me too)");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.Blue;
+            Console.WriteLine("Put your head on my shoulder");
+            Console.WriteLine("Whisper in my ear, baby");
+            Console.WriteLine("Words I want to hear, baby");
+            Console.WriteLine("Put your head on my shoulder");
+
+            Console.ForegroundColor=ConsoleColor.White;
+            
+        }
+    }
+}
